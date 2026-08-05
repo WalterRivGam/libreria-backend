1a1e9ff7538a4ee92f3eeec154f9351d1d0cd306
WalterRivGam <38467480+WalterRivGam@users.noreply.github.com>
2026-08-04 20:58:08 -0500

    agrega autenticación básica por base de datos

--- src/main/java/com/libreria/security/SecurityConfig.java
+++ src/main/java/com/libreria/security/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,9 +17,11 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http.csrf(csrf -> csrf.disable());
 
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
 
         http.httpBasic(Customizer.withDefaults());
+
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
---
+++ src/main/java/com/libreria/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package com.libreria.service.impl;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.libreria.dto.UsuarioDto;
+import com.libreria.repository.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UserDetailsServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UsuarioDto usuarioDto = usuarioRepository.obtenerUsuario(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+        return User
+                .withUsername(usuarioDto.getUsername())
+                .password(usuarioDto.getPassword())
+                .authorities(usuarioDto.getRol())
+                .accountExpired(false)
+                .accountLocked(false)
+                .credentialsExpired(false)
+                .disabled(!usuarioDto.getEnabled())
+                .build();
+
+    }
+
+}
