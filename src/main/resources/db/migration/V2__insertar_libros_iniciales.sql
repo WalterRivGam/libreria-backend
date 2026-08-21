e976ad4a83e31aadcd0b0e21704a5a698a7cbc22
WalterRivGam <38467480+WalterRivGam@users.noreply.github.com>
2026-08-21 18:12:11 -0500

    usa flyway para control de versiones de bases de datos

---
+++ src/main/resources/db/migration/V2__insertar_libros_iniciales.sql
@@ -0,0 +1,4 @@
+insert into libro (titulo, autor, precio) values
+('Harry Potter y la piedra filosofal', 'Rowling, J. K.', 59.25),
+('Antología del Terror', 'Lovecraft, H.P.', 48.75),
+('El Perro de los Baskerville', 'Conan Doyle, Arthur', 29.93);
