c13141993dc8f12e28d0be65481e578a676544de
WalterRivGam <38467480+WalterRivGam@users.noreply.github.com>
2026-09-04 10:20:46 -0500

    manejo de excepción de datos inválidos

--- src/main/java/com/libreria/controller/advice/LibroControllerAdvice.java
+++ src/main/java/com/libreria/controller/advice/LibroControllerAdvice.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import java.time.LocalDateTime;
 import jakarta.servlet.http.HttpServletRequest;
 import com.libreria.dto.ErrorDto;
+import com.libreria.exception.DatosInvalidosException;
 import com.libreria.exception.LibroNoEncontradoException;
 
 @RestControllerAdvice
@@ -22,5 +23,18 @@ public class LibroControllerAdvice {
                 request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DatosInvalidosException.class)
+    public ResponseEntity<ErrorDto> datosInvalidosExceptionHandler(DatosInvalidosException exception,
+            HttpServletRequest request) {
+        ErrorDto error = new ErrorDto(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                exception.getMessage(),
+                request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
